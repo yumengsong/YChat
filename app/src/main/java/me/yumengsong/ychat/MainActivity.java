1bfb5c2e9c0b04c8f7519c7c58300448d0e62142
Yumeng Song <ymeng.song@gmail.com>
2016-09-21 19:09:57 -0400

    Added the ToolBar and customzied the overflow button.
    Use Toolbar as an App Bar to provides a material design experience on devices running Android 2.1 (API level 7) or later.
    Require the v7 appcompat support library to implement.
    Changed the overflow button in the App Bar from three dots to an "add" icon.
    
    Use ToolBar instead of ActionBar to support API level lower than 11.

--- app/src/main/java/me/yumengsong/ychat/MainActivity.java
+++ app/src/main/java/me/yumengsong/ychat/MainActivity.java
@@ -1,13 +1,46 @@
 package me.yumengsong.ychat;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
 
-public class MainActivity extends Activity {
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /*Show Overflow option menu item icon*/
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method method = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    method.setAccessible(true);
+                    method.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
     }
 }
